3ed3d33297740aa75ce789c52c639c5b798798cc
panosrodo <brandonroots@hotmail.gr>
2025-06-01 17:59:48 +0300

    fix: add lombok annotations

--- src/main/java/gr/aueb/cf/teacherapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/teacherapp/dto/TeacherInsertDTO.java
@@ -3,7 +3,15 @@ package gr.aueb.cf.teacherapp.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TeacherInsertDTO {
 
     @NotNull(message = "Το όνομα δεν μπορεί να είναι null.")
