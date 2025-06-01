96ce6112732d2350f7f407089bfcd6916cc69b24
panosrodo <brandonroots@hotmail.gr>
2025-06-01 18:45:09 +0300

    fix: add mapper

---
+++ src/main/java/gr/aueb/cf/teacherapp/mapper/Mapper.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.teacherapp.mapper;
+
+import gr.aueb.cf.teacherapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.teacherapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.teacherapp.model.Teacher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapper {
+
+    public Teacher mapToTeacherEntity(TeacherInsertDTO teacherInsertDTO) {
+        Teacher teacher = new Teacher();
+        teacher.setFirstname(teacherInsertDTO.getFirstname());
+        teacher.setLastname(teacherInsertDTO.getLastname());
+        teacher.setVat(teacherInsertDTO.getVat());
+        return teacher;
+    }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(),
+                teacher.getUpdatedAt(), teacher.getUuid(), teacher.getFirstname(),
+                teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
+    }
+}
