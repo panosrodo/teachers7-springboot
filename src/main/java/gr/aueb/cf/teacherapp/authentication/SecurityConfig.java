f9c6dd0fdb05e611b56461f0c06f247ededda0eb
panosrodo <brandonroots@hotmail.gr>
2025-06-02 15:44:18 +0300

    feat: add security config

---
+++ src/main/java/gr/aueb/cf/teacherapp/authentication/SecurityConfig.java
@@ -0,0 +1,43 @@
+package gr.aueb.cf.teacherapp.authentication;
+
+import gr.aueb.cf.teacherapp.core.enums.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/index.html").permitAll()
+                        .requestMatchers("/school/users/register").permitAll()
+                        .requestMatchers("/school/teachers/**").hasAnyAuthority(Role.TEACHER.name())
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/school/teachers", false)
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())       // username, password and session cookies
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+        return http.build();
+    }
+}
